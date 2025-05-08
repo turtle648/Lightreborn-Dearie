47d759c3097419188d11f5290c4661aa181fdde1
조희진 <allana123@naver.com>
2025-05-08 10:06:13 +0900

    feat: 월별 일별 상담일지 가져오기 완료 (#37, #S12P31S309-223, #S12P31S309-224)

--- lightreborn/backend/src/main/java/com/ssafy/backend/youth_consultation/service/YouthConsultationService.java
+++ lightreborn/backend/src/main/java/com/ssafy/backend/youth_consultation/service/YouthConsultationService.java
@@ -1,14 +1,12 @@
 package com.ssafy.backend.youth_consultation.service;
 
-import com.ssafy.backend.youth_consultation.model.dto.request.AddScheduleRequestDTO;
-import com.ssafy.backend.youth_consultation.model.dto.request.PeopleInfoRequestDTO;
-import com.ssafy.backend.youth_consultation.model.dto.request.SpeechRequestDTO;
-import com.ssafy.backend.youth_consultation.model.dto.request.UpdateCounselingLogRequestDTO;
+import com.ssafy.backend.youth_consultation.model.dto.request.*;
 import com.ssafy.backend.youth_consultation.model.dto.response.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface YouthConsultationService {
     GetCounselingLogResponseDTO getCounselingLog(int pageNum, int sizeNum);
+    GetCounselingLogResponseDTO getMonthlyCounselingLog(GetMonthlyCounselingLogDTO request);
     PeopleInfoResponseDTO searchPeopleInfo(PeopleInfoRequestDTO peopleInfoRequestDTO);
     AddScheduleResponseDTO addSchedule(Long id, AddScheduleRequestDTO addScheduleRequestDTO);
     SpeechResponseDTO getGeneralSummarize(SpeechRequestDTO responseDTO);
