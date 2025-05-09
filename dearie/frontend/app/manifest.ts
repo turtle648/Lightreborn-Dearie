0d70b11c585b3296475e24ad4cf3b3eb769c049d
문영민 <ansdudals8@naver.com>
2025-05-09 21:28:48 +0900

    Chore: pwa 경로 설정

--- dearie/frontend/app/manifest.ts
+++ dearie/frontend/app/manifest.ts
@@ -5,19 +5,20 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Dearie - 일기기반 정신건강 자가진단 앱",
     short_name: "Dearie",
     description: "일기를 통한 정신건강 자가진단 앱",
-    start_url: "/",
+    start_url: "/dearie/",
+    scope: "/dearie/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#f1b29f",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/dearie/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/dearie/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
