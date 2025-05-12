819eaca000dbcc1d56ef89202dae85c24edea73f
조원주 <jowonju8@gmail.com>
2025-05-12 12:36:39 +0900

    Feat: S3 설정 추가

---
+++ dearie/backend/src/main/java/com/ssafy/backend/common/config/S3Config.java
@@ -0,0 +1,49 @@
+package com.ssafy.backend.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Utilities;
+
+import java.time.Duration;
+
+@Configuration
+public class S3Config {
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3AsyncClient s3AsyncClient() {
+        return S3AsyncClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.builder()
+                                .apiCallTimeout(Duration.ofSeconds(30))
+                                .build()
+                )
+                .build();
+    }
+
+    @Bean
+    public S3Utilities s3Utilities() {
+        return S3Utilities.builder()
+                .region(Region.of(region))
+                .build();
+    }
+}
