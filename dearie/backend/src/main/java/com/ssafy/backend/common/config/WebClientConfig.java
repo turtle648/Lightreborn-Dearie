fdf5887db4b09eca3fc4a29585bf62eb459608bd
문영민 <ansdudals8@naver.com>
2025-05-14 12:31:58 +0900

    Chore: 설정 변경

--- dearie/backend/src/main/java/com/ssafy/backend/common/config/WebClientConfig.java
+++ dearie/backend/src/main/java/com/ssafy/backend/common/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${yolo.server.base-url}")
+//    @Value("${yolo.server.base-url}")
     private String BASE_URL;
 
     @Bean
