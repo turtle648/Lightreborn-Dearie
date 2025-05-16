82d14fc2f542748a93b1b728f7a2754fcc8cff15
조희진 <allana123@naver.com>
2025-05-16 17:13:25 +0900

    hotfix: builder 추가

--- dearie/backend/src/main/java/com/ssafy/backend/mission/model/dto/response/DailyMissionResponseDTO.java
+++ dearie/backend/src/main/java/com/ssafy/backend/mission/model/dto/response/DailyMissionResponseDTO.java
@@ -2,10 +2,14 @@ package com.ssafy.backend.mission.model.dto.response;
 
 import com.ssafy.backend.mission.model.enums.MissionTypeMapping;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DailyMissionResponseDTO {
     private Long Id;
     private Long missionId;
