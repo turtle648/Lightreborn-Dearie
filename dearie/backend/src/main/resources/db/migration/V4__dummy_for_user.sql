cbc0988e0a4624f9cce6a5a5729eb9b366dbeb18
문영민 <ansdudals8@naver.com>
2025-05-20 14:52:43 +0900

    Chore: dummy-mission

---
+++ dearie/backend/src/main/resources/db/migration/V4__dummy_for_user.sql
@@ -0,0 +1,21 @@
+-- user_mission 넣기 
+INSERT INTO user_missions (date, is_completed, user_id, mission_id) VALUES
+('2025-05-20', FALSE, 3, 1),
+('2025-05-20', FALSE, 3, 2),
+('2025-05-20', FALSE, 3, 4),
+('2025-05-20', FALSE, 3, 6),
+('2025-05-20', FALSE, 3, 7);
+
+INSERT INTO user_missions (date, is_completed, user_id, mission_id) VALUES
+('2025-05-20', FALSE, 4, 1),
+('2025-05-20', FALSE, 4, 2),
+('2025-05-20', FALSE, 4, 4),
+('2025-05-20', FALSE, 4, 5),
+('2025-05-20', FALSE, 4, 7);
+
+INSERT INTO user_missions (date, is_completed, user_id, mission_id) VALUES
+('2025-05-20', FALSE, 5, 1),
+('2025-05-20', FALSE, 5, 2),
+('2025-05-20', FALSE, 5, 4),
+('2025-05-20', FALSE, 5, 6),
+('2025-05-20', FALSE, 5, 7);
