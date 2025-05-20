f8b172f56a596899d16e03cdcf51ca1f83999f26
문영민 <ansdudals8@naver.com>
2025-05-20 15:35:59 +0900

    Chore: 더미 데이터 추가

---
+++ dearie/backend/src/main/resources/db/migration/V5__dummy_for_wonju.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_missions (date, is_completed, user_id, mission_id) VALUES
+('2025-05-20', FALSE, 6, 1),
+('2025-05-20', FALSE, 6, 2),
+('2025-05-20', FALSE, 6, 4),
+('2025-05-20', FALSE, 6, 6),
+('2025-05-20', FALSE, 6, 7);
